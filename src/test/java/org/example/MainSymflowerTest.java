952a994a7d617dc067aff65c06acdfabaf50935d
Muslima Akhrorova <muslima@Muslimas-MBP.home>
2023-02-25 15:10:10 -0500

    Initial commit

---
+++ src/test/java/org/example/MainSymflowerTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.junit.jupiter.api.*;
+
+public class MainSymflowerTest {
+	@Test
+	public void test11() throws InterruptedException {
+		Main m = null; // TODO This is a fallback value due to incomplete analysis.
+		m.test1();
+	}
+}
